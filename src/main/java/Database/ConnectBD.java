50e6caa38ba0dd781b3db4d0aa5df9eafb087eaa
Amidou DIAGANA <diaganaamidou@gmail.com>
2026-05-22 12:06:51 +0000

    Commit1

--- src/main/java/Database/ConnectBD.java
+++ src/main/java/Database/ConnectBD.java
@@ -9,9 +9,10 @@ public class ConnectBD {
     private static String URL="jdbc:mysql://localhost:3306/advisor";
     private static String USER="root";
     private static String PASSWORD="diallacoul";
-    private static Connection getConnection () throws SQLException {
-        return DriverManager.getConnection(URL,USER,PASSWORD);
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
 }
 
 
---
+++ src/main/java/model/service/InterfaceEtape.java
@@ -0,0 +1,19 @@
+package model.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import model.classes.Etape;
+
+
+//Interface de Service pour gérer les opérations liées aux étapes
+public interface InterfaceEtape {
+
+    boolean ajoutt(Etape etape);
+    Optional<Etape> etape(int idEtape);
+    List<Etape> Les_etapes();
+    boolean miseAjour(Etape etape);
+    boolean suppression(int idEtape);
+    boolean verification(int idEtape);
+
+}
